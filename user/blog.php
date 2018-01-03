fea94a2df7cbe120ddac56ab78cf92cb9b7816f1
lmucassi <lindasmucassi@gmai.com>
2018-01-03 12:45:58 +0200

    updates

--- user/blog.php
+++ user/blog.php
@@ -26,7 +26,6 @@
             <h3 class="cam-h3">Camagru Cam</h3>
             <!-- camera -->
             <div class="app">
-                    <a href="#" id="start-camera" class="visible">Touch here to start the app.</a>
                     <div class="row">
                         <div class="col-9" id="main">
                             <div class="row">
@@ -66,7 +65,6 @@
                     </div>
                     <div class="blog-summary">
                         <p>I love working on fresh designs that
-                            <a href="https://www.youtube.com/watch?v=hANtM1vJvOo">breathe</a>.
                             To that end, I need to freshen up my portfolio here because
                             it does exactly the opposite. For the next month I will be working
                             almost exclusively on my portfolio. Sounds like a lot of fun!</p>
@@ -105,7 +103,6 @@
                     </div>
                     <div class="blog-summary">
                         <p>I love working on fresh designs that
-                            <a href="https://www.youtube.com/watch?v=hANtM1vJvOo">breathe</a>.
                             To that end, I need to freshen up my portfolio here because
                             it does exactly the opposite. For the next month I will be working
                             almost exclusively on my portfolio. Sounds like a lot of fun!</p>
@@ -145,7 +142,6 @@
                     </div>
                     <div class="blog-summary">
                         <p>I love working on fresh designs that
-                            <a href="https://www.youtube.com/watch?v=hANtM1vJvOo">breathe</a>.
                             To that end, I need to freshen up my portfolio here because
                             it does exactly the opposite. For the next month I will be working
                             almost exclusively on my portfolio. Sounds like a lot of fun!</p>
@@ -185,7 +181,6 @@
                     </div>
                     <div class="blog-summary">
                         <p>I love working on fresh designs that
-                            <a href="https://www.youtube.com/watch?v=hANtM1vJvOo">breathe</a>.
                             To that end, I need to freshen up my portfolio here because
                             it does exactly the opposite. For the next month I will be working
                             almost exclusively on my portfolio. Sounds like a lot of fun!</p>
