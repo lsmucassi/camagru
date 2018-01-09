ab4f372b4c5a5bbd6ab0e685cafddad06bbe6705
lmucassi <lindasmucassi@gmai.com>
2018-01-09 11:32:35 +0200

    updates

--- headnfoot/footer.php
+++ headnfoot/footer.php
@@ -5,48 +5,48 @@
       <h4 class="footer-list-header">About Camagru</h4></li>
     <li><a href="about.php" class="generic-anchor footer-list-anchor" itemprop="significantLink">WHAT THE BYTE IS THIS</a></li>
     <li><a href="camagru pdf" class="generic-anchor footer-list-anchor" itemprop="significantLink">SPECIFICATIONS</a></li>
-    <li><a href='/retailers/new-retailers.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">RESOURCES</a></li>
-    <li><a href='/job-openings.html' itemprop="significantLink" class="generic-anchor footer-list-anchor">WANT MORE</a></li>
-    <li><a href='/shop/about-show-schedule' class="generic-anchor footer-list-anchor" itemprop="significantLink">HEY! CHECK THIS OUT</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">RESOURCES</a></li>
+    <li><a href="" itemprop="significantLink" class="generic-anchor footer-list-anchor">WANT MORE</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">HEY! CHECK THIS OUT</a></li>
   </ul>
 
   <ul class="footer-list-top">
     <li>
       <h4 class="footer-list-header">The Gift Selection</h4></li>
-    <li><a href='/Angels/cat/id/70' class="generic-anchor footer-list-anchor">ANGEL FIGURINES</a></li>
-    <li><a href='/Home-Decor/cat/id/64' class="generic-anchor footer-list-anchor">HOME DECOR</a></li>
-    <li><a href='/Mugs/cat/id/32' class="generic-anchor footer-list-anchor">MUGS</a></li>
-    <li><a href='/Pet-Lover/cat/id/108' class="generic-anchor footer-list-anchor">PET LOVER</a></li>
-    <li><a href='/Ladies-Accessories/cat/id/117' class="generic-anchor footer-list-anchor" target="_blank">HANDBAGS & JEWELRY</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor">ANGEL FIGURINES</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor">HOME DECOR</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor">MUGS</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor">PET LOVER</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor" target="_blank">HANDBAGS & JEWELRY</a></li>
   </ul>
   
   <ul class="footer-list-top">
     <li id='help'>
       <h4 class="footer-list-header">Please Help Me</h4></li>
-    <li><a href='/shop/about-contact' class="generic-anchor footer-list-anchor" itemprop="significantLink">CONTACT</a></li>
-    <li><a href='/faq.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">FAQ</a></li>
-    <li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor footer-list-anchor" itemprop="significantLink">STORE LOCATOR</a></li>
-    <li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor footer-list-anchor" itemprop="significantLink">NEW USERS</a></li>
-    <li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor footer-list-anchor">ORDER STATUS</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">CONTACT</a></li>
+    <li><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">FAQ</a></li>
+    <li id='find-a-store'><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">STORE LOCATOR</a></li>
+    <li id='user-registration'><a href="" class="generic-anchor footer-list-anchor" itemprop="significantLink">NEW USERS</a></li>
+    <li id='order-tracking'><a href="" itemprop="significantLink" class="generic-anchor footer-list-anchor">ORDER STATUS</a></li>
   </ul>
   
   <section class="footer-social-section flex-rw">
       <span class="footer-social-overlap footer-social-connect">
-      CONNECT <span class="footer-social-small">with</span> US
+      CONNECT <span class="footer-social-small">with</span> Me
       </span>
       <span class="footer-social-overlap footer-social-icons-wrapper">
-      <a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
-      <a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fa fa-twitter"></i></a>
-      <a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fa fa-instagram"></i></a>
-      <a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fa fa-youtube"></i></a>
-      <a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
+      <a href="https://www.facebook.com/lmucassi" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/lsmucassi" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fa fa-twitter"></i></a>
+      <a href="http://instagram.com/linda_mucassi" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="material-icons">party_mode</i></a>
+      <a href="https://www.youtube.com/channel/UCW9nb8vPqgHO6rxGbILb3Rw?view_as=subscriber" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fa fa-youtube"></i></a>
+      <a href="https://plus.google.com/u/0/100306940019861827760" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
       </span>
   </section>
   
   <section class="footer-bottom-section flex-rw">
 	<div class="footer-bottom-wrapper">   
 		<i class="fa fa-copyright" role="copyright">
-		</i> 2015 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
+		</i> 2018 Camagru at <address class="footer-address" role="company address">wethinkcode_, JHB</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
     </div>
 
     <div class="footer-bottom-wrapper">
